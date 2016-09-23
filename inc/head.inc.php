1f49bbe2c192965d3311eeeb5b4b06a5d49e158c
kazes <florian.boudot@gmail.com>
2016-09-23 16:55:22 +0200

    first commit : html startkit structure

---
+++ inc/head.inc.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/all.css" type="text/css">
+
+<title> | My project title</title>
+<script>
+    var hasJS = function() {
+        var htmlTag = document.getElementsByTagName('html')[0];
+        htmlTag.className = (htmlTag.className + ' ' || '') + 'hasJS';
+    }();
+</script>
